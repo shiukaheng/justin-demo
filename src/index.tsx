0e07a65c5ba7be3f61a061c0b0ddd5c06cfa1b9a
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-06-16 19:02:16 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { App } from "./App";
+import { createRoot } from 'react-dom/client';
+import "./index.css"
+
+// Import css
+const root = createRoot(document.getElementById('root'));
+root.render(<App/>)
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': 'Nighty',
+      'sans': 'Archivo Black'
+    }
+  },
+  plugins: [],
+}
